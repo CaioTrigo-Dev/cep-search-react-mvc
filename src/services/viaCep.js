bf87e2d5cca5010266a562fa9dba7e4e269cd6ad
Caio Cesar <caiorj95@gmail.com>
2026-01-22 21:37:29 -0300

    feature: adicionando  ViaCEP

---
+++ src/services/viaCep.js
@@ -0,0 +1,17 @@
+export default async function getPostalCodeData(cep){
+  const cleanCep = cep.replace(/\D/g, '');
+
+ const response = await fetch(`https://viacep.com.br/ws/${cleanCep}/json/`);
+
+ if(!response.ok){
+  throw new Error('Erro ao conectar com o Serviço');
+ }
+
+ const data = await response.json();
+
+ if(data.erro){
+  throw new Error('CEP não encontrado');
+ }
+
+ return data;
+}
